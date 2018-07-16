8a1d1f345e880b8dcaaea8fe3359deac86e1dc7d
sanhs <santhoshkapuluru@gmail.com>
2018-07-16 10:34:37 +0530

    first commit

---
+++ simple-login-web/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import './index.css';
+import Root from './root';
+import rootReducer from './reducers';
+
+const initialState = {};
+const middleware = [thunk];
+const devTools = process.env.NODE_ENV = ('development') ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : null;
+const store = createStore(
+    rootReducer,
+    initialState,
+    compose(applyMiddleware(...middleware), devTools)
+);
+
+ReactDOM.render(<Root store = {store}/>, document.getElementById('root'));
